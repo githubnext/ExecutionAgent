0c053071f08309f2f070581b0a3fdb11ca36f676
David Slater <davidslater@users.noreply.github.com>
2024-12-18 22:14:04 +0000

    update exp setups

--- experimental_setups/increment_experiment.py
+++ experimental_setups/increment_experiment.py
@@ -1,8 +1,8 @@
 import os
 
 with open("experimental_setups/experiments_list.txt", "r+") as expl:
-    exps = expl.read().splitlines()
-    #print(exps)
+    exps = [x for x in expl.read().splitlines() if x.strip()]
+    # print(exps)
     if exps:
         last_exp = int(exps[-1].split("_")[1])
     else:
@@ -13,7 +13,7 @@ with open("experimental_setups/experiments_list.txt", "r+") as expl:
     os.mkdir("experimental_setups/experiment_{}".format(last_exp + 1))
     os.mkdir("experimental_setups/experiment_{}/logs".format(last_exp + 1))
     os.mkdir("experimental_setups/experiment_{}/responses".format(last_exp + 1))
-    #os.mkdir("experimental_setups/experiment_{}/external_fixes".format(last_exp + 1))
+    # os.mkdir("experimental_setups/experiment_{}/external_fixes".format(last_exp + 1))
     os.mkdir("experimental_setups/experiment_{}/saved_contexts".format(last_exp + 1))
-    #os.mkdir("experimental_setups/experiment_{}/mutations_history".format(last_exp + 1))
+    # os.mkdir("experimental_setups/experiment_{}/mutations_history".format(last_exp + 1))
     os.mkdir("experimental_setups/experiment_{}/files".format(last_exp + 1))
